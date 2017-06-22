9f40f5cd5ad0424b9de8e88416174a64f43d04fa
AlexanderAnishchik <sanyaaxel94@gmail.com>
2017-06-22 20:27:41 +0300

    init

---
+++ Angular4/wwwroot/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    template: '<div>{{user}} angular4</div>',
+    selector: 'app'
+})
+export class AppComponent {
+    public user: string = "Test";
+}
---
+++ Angular4/wwwroot/app/app.module.ts
@@ -0,0 +1,12 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    bootstrap: [AppComponent],
+    imports: [BrowserModule],
+    declarations:[AppComponent]
+})
+export class AppModule {    
+
+}
